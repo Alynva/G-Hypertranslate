eefd9d6fb6edea0732c9a8114c9701fea120c205
Alisson Nunes <alynva@gmail.com>
2022-09-07 15:18:16 -0300

    Using undici dependency

--- src/translate.js
+++ src/translate.js
@@ -1,3 +1,5 @@
+import { request } from "undici"
+
 export const supportedLangs = ["af", "sq", "am", "ar", "hy", "az", "eu", "bn", "bs", "bg", "ca", "ceb", "zh-CN", "zh-TW", "co", "hr", "cs", "da", "nl",
 	"en", "eo", "et", "fi", "fr", "fy", "gl", "ka", "de", "el", "gu", "ht", "ha", "haw", "iw", "hi", "hmn", "hu", "is", "id", "ga",
 	"ja", "jw", "kn", "kk", "km", "ko", "ku", "lo", "lv", "lt", "lb", "mk", "mg", "ms", "ml", "mt", "mi", "mr", "mn", "ne", "no", "ny",
@@ -21,8 +23,8 @@ export default async function translate(text, sourceLang, targetLang) {
 
 	let url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sourceLang}&tl=${targetLang}&dt=t&q=${encodeURI(text)}`
 
-	const [result, lang] = await fetch(url)
-		.then(r => r.json())
+	const [result, lang] = await request(url)
+		.then(({ body }) => body.json())
 		.then(response => {
 			const data = response[0]
 			let result = ''
